0a67ed287ddb958a3ca2893b55a7828211f7859c
MichaloxBruh <79116858+MichaloxBruh@users.noreply.github.com>
2024-02-02 16:06:12 +0100

    username

--- LoginTest/Program.cs
+++ LoginTest/Program.cs
@@ -1,8 +1,10 @@
 ﻿
 using Spectre.Console;
 
+string fileName1 = @"D:\CSFiles_mf\Username_fake_mf_mf.txt";
 string fileName = @"D:\CSFiles_mf\Password_fake_mf_mf.txt";
-string password_real = "";
+string username1_real = "";
+string password1_real = "";
 int logintries = 1;
 try
 {
@@ -13,14 +15,28 @@ try
 
     using (StreamWriter sw = File.CreateText(fileName))
     {
+        
         sw.WriteLine("pizza");
     }
 
     using (StreamReader sr = File.OpenText(fileName))
     {
-        password_real = "";
-        password_real = sr.ReadLine();
-        LoginMode();
+        password1_real = "";
+        password1_real = sr.ReadLine();
+        UserLogin();
+    }
+
+    using (StreamWriter sw = File.CreateText(fileName1))
+    {
+
+        sw.WriteLine("Michalox");
+    }
+
+    using (StreamReader sr = File.OpenText(fileName1))
+    {
+        username1_real = "";
+        username1_real = sr.ReadLine();
+        UserLogin();
     }
 }
 catch (Exception Ex)
@@ -28,11 +44,34 @@ catch (Exception Ex)
     Console.WriteLine(Ex.ToString());
 }
 
+void UserLogin()
+{
+    string Username_written = AnsiConsole.Ask<string>("Username:");
+
+    if (Username_written.Equals(username1_real))
+    {
+        Console.WriteLine("User Found");
+        LoginMode();
+    }
+    //else if (Username_written.Equals(username2_real))
+    else
+    {
+        Console.WriteLine("Wrong Username!");
+    }
+
+
+}
+
+
+
+
+
+
 void LoginMode()
 {
-    string password_written = AnsiConsole.Ask<string>("Write your password");
+    string password_written = AnsiConsole.Ask<string>("Password");
 
-    if (password_written.Equals(password_real))
+    if (password_written.Equals(password1_real))
     {
         Console.WriteLine("Welcome :D");
     }
@@ -40,7 +79,7 @@ void LoginMode()
     {
         Console.WriteLine("Too many tries, reopen the program");
     }
-    else if (!password_written.Equals(password_real))
+    else if (!password_written.Equals(password1_real))
     {
         Console.WriteLine("Wrong Password, " + logintries + " attemps done (Max 3)");
         logintries = logintries + 1;
